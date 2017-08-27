984872fffcef3ee78b1fb6026cca4bd14ad654c2
jhonRawls <jhonRawls@gmial.com>
2017-08-27 20:12:27 +0800

    代码提交

---
+++ web_api/src/main/java/cn/ibadi/web_api/WebApiApplication.java
@@ -0,0 +1,12 @@
+package cn.ibadi.web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiApplication.class, args);
+	}
+}
---
+++ web_api/src/main/java/cn/ibadi/web_api/controller/UserController.java
@@ -0,0 +1,15 @@
+package cn.ibadi.web_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String signIn() {
+		return "signIn";
+	}
+
+}
