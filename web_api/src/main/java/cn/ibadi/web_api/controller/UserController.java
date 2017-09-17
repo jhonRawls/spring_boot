54e18f9cd6f37de1a8a63e5239b6272f9b74112c
jhonRawls <jhonRawls@gmial.com>
2017-09-17 15:47:03 +0800

    commit ...

--- web_api/src/main/java/cn/ibadi/web_api/controller/UserController.java
+++ web_api/src/main/java/cn/ibadi/web_api/controller/UserController.java
@@ -1,5 +1,7 @@
 package cn.ibadi.web_api.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("user")
 public class UserController {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String signIn() {
+		logger.info("访问登录页面");
 		return "signIn";
 	}
 
